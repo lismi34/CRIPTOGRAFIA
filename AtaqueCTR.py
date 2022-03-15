06cf287b1bea2afb727be1ca7fed65b273b60a3b
lismi34 <101617112+lismi34@users.noreply.github.com>
2022-03-15 11:42:05 -0600

    .
    .

--- AtaqueCTR.py
+++ AtaqueCTR.py
@@ -1,69 +1,84 @@
 import os
+import argparse
+import base64
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 def calcular_xor(binario1, binario2):
-	bytes1 = list(binario1)
-	bytes2 = list(binario2)
-	longitud_menor = len(bytes1)
-	lista_larga = bytes2
+    bytes1 = list(binario1)
+    bytes2 = list(binario2)
+    longitud_menor = len(bytes1)
+    lista_larga = bytes2
 
-	if len(bytes2) < longitud_menor:
-		longitud_menor = len(bytes2)
-		lista_larga = bytes1
+    if len(bytes2) < longitud_menor:
+        longitud_menor = len(bytes2)
+        lista_larga = bytes1
 
-	res_bytes=[]
+    res_bytes=[]
 
-	for i in range(longitud_menor):
-		res_bytes.append(bytes1[i] ^ bytes2[i])
+    for i in range(longitud_menor):
+        res_bytes.append(bytes1[i] ^ bytes2[i])
 
-	return bytes(res_bytes) + bytes(lista_larga[longitud_menor:])
+    return bytes(res_bytes) + bytes(lista_larga[longitud_menor:])
 
-
-def descifrar(entrada, key, iv):
-    aesCipher = Cipher(algorithms.AES(key),
-                       modes.CTR(iv),
-                       backend = default_backend)
-
-    aesDecryptor = aesCipher.decryptor()
-    plano = aesDecryptor.update(entrada)
-    print("P:", plano)
-    aesDecryptor.finalize()
-    return plano
-  
-def cifrar(entrada, llave, nonce):
+def cifrar(llave, nonce):
+    i = 0
     aesCipher = Cipher(algorithms.AES(llave),
                        modes.CTR(nonce),
                        backend = default_backend)
     aesEncryptor = aesCipher.encryptor()
-    c = aesEncryptor.update(entrada)
-    print(c)
+
+    salida = open('./nuevo.cif', 'wb')
+    for buffer in open('./atacante.xml', 'rb'):
+        print("BUFFER:",buffer)
+        cifrado = aesEncryptor.update(buffer)
+        if i == 2:
+            key_stream = calcular_xor(cifrado, buffer)
+            texto_falso= b'    <Merchant>Liset MI</Merchant>\n'
+            cifrado = calcular_xor(texto_falso, key_stream)
+
+        salida.write(cifrado)
+        i = i+1
+
     aesEncryptor.finalize()
-    return c 
+    salida.close()
 
 
+def descifrar(llave, nonce):
+    aesCipher = Cipher(algorithms.AES(llave),
+                       modes.CTR(nonce),
+                       backend = default_backend)
+    aesDecryptor = aesCipher.decryptor()
 
-key = os.urandom(16)
-iv = os.urandom(16)
+    salida = open('./nuevo.xml', 'wb')
+    for buffer in open('./nuevo.cif', 'rb'):
+        plano= aesDecryptor.update(buffer)
+        salida.write(plano)
+        
+    aesDecryptor.finalize()
+    salida.close()
 
-i = 0
-salida = open("salida.cif", 'wb')
-for buffer in open('./atacante.xml', 'rb'):
-	if i == 2:
-		key_stream = calcular_xor(cifrado, descifrado)
-		print(key_stream)
-		texto_falso= b'<Merchant>Liset MI</Merchant>'
-		key=key_stream
-		cifrado = cifrar(texto_falso, key, iv)
-		descifrado = descifrar(cifrado, key, iv)
-	else:
-		cifrado = cifrar(buffer, key, iv)
-		descifrado = descifrar(cifrado, key, iv)
+if __name__ == '__main__':
+    all_args =  argparse.ArgumentParser()
+    all_args.add_argument("-p", "--Operacion", help="Aplicar operación, cifrar/descifrar")
+    all_args.add_argument("-l", "--llave", help="Llave", required=True)
+    all_args.add_argument("-n", "--Nonce", help="IV", required=True)
+    
+    args = vars(all_args.parse_args())
+    operacion = args['Operacion']
 
-	salida.write(cifrado)
-		
+    # Preparar llave recibida en base64
+    key = base64.b64decode(args['llave'])
+    iv = base64.b64decode(args['Nonce'])
 
-	i=i+1	
+    print(len(key))
+    if (len(key) != 16) or (len(iv) != 16):
+        print('Verificar tamaño de la Llave o IV, debe ser de 16 bytes')
+        exit()
+    
+    if operacion == 'cifrar':
+        cifrar(key, iv)
+    else:
+        descifrar(key, iv)
 
-salida.close()
 
